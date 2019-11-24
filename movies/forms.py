97990b6e99da6d054276b2abfba7de0835a4d7f3
toohong5 <toohong5@gmail.com>
2019-11-24 17:37:56 +0900

    Complete model

--- movies/forms.py
+++ movies/forms.py
@@ -6,7 +6,7 @@ from .models import Movie, Genre, Rating
 class MovieForm(forms.ModelForm):
     class Meta:
         model = Movie
-        fields = ('title', 'original_title', 'genre_ids', 'release_date',
+        fields = ('title', 'original_title', 'genres', 'release_date',
                     'popularity', 'overview', 'director', 'runtime', 'video')
 
 
